d46815712399aecfe507460723f183bb15c2f6c4
Dominik Ferber <dominik.ferber@gmail.com>
2016-07-27 19:37:10 +0200

    version 2.2.2_1
    - upgrade to 2.2.2
    - add protocol support
    
    closes #21

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'dferber:prerender',
   summary: 'Node wrapper for prerender',
-  version: '2.2.0_2',
+  version: '2.2.2_1',
   git: 'https://github.com/dferber90/meteor-prerender',
 });
 
 Npm.depends({
-  'prerender-node': '2.2.0',
+  'prerender-node': '2.2.2',
 });
 
 Package.onUse(function packageConfiguration(api) {
