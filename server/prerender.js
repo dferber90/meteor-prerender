02aa8c04810b11058c1db302f748ac591b288023
Almog Koren <almog@almog.io>
2016-07-27 16:39:45 +0300

    Added protocol
    Added protocol to support sites and apps using https

--- server/prerender.js
+++ server/prerender.js
@@ -1,18 +1,24 @@
 var prerenderio = Npm.require('prerender-node');
 var token;
 var serviceUrl;
+var protocol;
 var settings = Meteor.settings.PrerenderIO;
 
 // token
 token = process.env.PRERENDERIO_TOKEN || (settings && settings.token);
 
+
 // service url (support `prerenderServiceUrl` (for historical reasons) and `serviceUrl`)
 serviceUrl = settings && (settings.prerenderServiceUrl || settings.serviceUrl);
 serviceUrl = process.env.PRERENDERIO_SERVICE_URL || serviceUrl;
 
+// protocol
+protocol = process.env.PRERENDERIO_PROTOCOL || (settings && settings.protocol);
+
 if (token) {
   if (serviceUrl) prerenderio.set('prerenderServiceUrl', serviceUrl);
   prerenderio.set('prerenderToken', token);
+  prerenderio.set('prerenderProtocol', protocol);
 
   prerenderio.set('afterRender', function afterRender(error) {
     if (error) {
